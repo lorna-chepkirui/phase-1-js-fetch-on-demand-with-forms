392fd672e687592902a6901ccd00f44ae7450fba
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-17 09:39:36 +0300

    commit tree f6aa830c5a6312f53a35c48d6a55560040f6dd52

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+    const form = document.querySelector('form');
+    const inputField = document.querySelector('#searchByID')
+    form.addEventListener('submit', (event) =>{
+      event.preventDefault();
+      fetch( `http://localhost:3000/movies/${inputField.value }` )
+        .then( res => res.json() )
+        .then( data =>{
+          const titleHtml = document.querySelector('#titleHtml');
+          const summaryHtml = document.querySelector('#summaryHtml')
+          titleHtml.innerHTML = data.title;
+          summaryHtml.innerHTML = data.summary;
+        });
+  
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init); 
